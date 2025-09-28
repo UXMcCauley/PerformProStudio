e20f3366c7fa176695e06f2dd8967095f90a9373
drewfsc <dmccauley@fsc-corp.org>
2025-09-27 23:47:43 -0500

    Upgrade Tailwind CSS and PostCSS dependencies for enhanced styling capabilities
    - Updated Tailwind CSS to version 4.0.0, improving utility classes and overall performance.
    - Introduced @tailwindcss/postcss for better integration with PostCSS, streamlining the build process.
    - Removed deprecated configurations in postcss.config.mjs to align with the latest standards.
    
    These updates enhance our styling framework, ensuring a more efficient and modern development experience.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,8 @@
 export default {
   plugins: {
-    tailwindcss: {
+    '@tailwindcss/postcss': {
       config: './tailwind.config.ts',
     },
-    // daisyui: {
-    //   darkMode: 'theme',
-    //   base: true,
-    //   styled: true,
-    //   utils: true,
-    //   prefix: '',
-    //   logs: true,
-    //   themeRoot: ':root',
-    //   themes: ['light', 'dark', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'aqua', 'luxury', 'dracula', 'night', 'coffee', 'winter'],
-    // },
     autoprefixer: {},
   },
 };
